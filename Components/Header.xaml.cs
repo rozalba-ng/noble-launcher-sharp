baefbe2675d7205dca6ab31c69052e163a079cd0
Albert <friendsnowy@gmail.com>
2022-02-13 02:18:03 +0500

    Close button click handler

--- Components/Header.xaml.cs
+++ Components/Header.xaml.cs
@@ -5,9 +5,6 @@ using System.Windows.Controls;
 
 namespace NoblegardenLauncherSharp.Components
 {
-    /// <summary>
-    /// Логика взаимодействия для Header.xaml
-    /// </summary>
     public partial class Header : UserControl
     {
         public Header() {
@@ -15,8 +12,11 @@ namespace NoblegardenLauncherSharp.Components
         }
 
         private void ToggleSettingsVisibility(object sender, RoutedEventArgs e) {
-            //SettingsBlockController.ToggleVisibility();
             EventDispatcher.Dispatch(EventDispatcherEvent.SettingsButtonClick);
+        }
+
+        private void CloseClick(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            Application.Current.Shutdown();
         }
     }
 }
